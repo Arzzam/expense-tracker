e0cee26c768e08ee1dfa0cbb0bd21f98824447fb
Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-02-01 15:17:49 +0530

    changes

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>31 Jan 2022</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">Car Insurance</h2>
+        <div className="expense-item__price">₹10000</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
