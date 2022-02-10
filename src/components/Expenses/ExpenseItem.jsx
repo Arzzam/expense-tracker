b11b37d65d8b6c78cd31480061e28644e9619b43
Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-02-10 19:51:51 +0530

    deleted

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -11,6 +11,12 @@ function ExpenseItem(props) {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">₹{props.amount}</div>
+        {/* <div
+          className="expense-item__price"
+          onClick={() => console.log()}
+        >
+          Delete
+        </div> */}
       </div>
     </Card>
   );
--- src/components/NewExpense/AddNewExpense.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import ExpenseForm from "./ExpenseForm";
-
-function AddNewExpense(props) {
-  return <button onClick={expand}>Add New Expense</button>;
-}
-
-export default AddNewExpense;
